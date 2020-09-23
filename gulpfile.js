0f69e7c1471cf0f526df3b9dde1f1a1abbe5d287
周青 <448340778@qq.com>
2020-09-23 15:36:35 +0800

    fix: gulp&node 报错

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,42 @@
 const gulp = require('gulp');
+
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cssmin = require('gulp-cssmin');
 const webpack = require('webpack');
 const webpackConf = require('./build/build.js');
+ 
 // 构建webpack配置
-gulp.task('webpack', function () {
-  webpack(webpackConf, function (err, stats) {
+function wp(cb) {
+  webpack(webpackConf, function(err, stats) {
     if (err) {
       console.log(err);
     }
+    cb();
   });
-});
+}
 
 // 处理样式的配置
-gulp.task('compile', function () {
-  return gulp
-    .src('./styles/index.scss')
-    .pipe(sass.sync())
-    .pipe(
-      autoprefixer({
-        browsers: ['ie > 9', 'last 2 versions'],
-        cascade: false
-      })
-    )
-    .pipe(cssmin())
-    .pipe(gulp.dest('./lib/'));
-});
+function compile(cb) {
+  return (
+    gulp
+      .src('./styles/index.scss')
+      .pipe(sass.sync())
+      .pipe(
+        autoprefixer({
+          browsers: ['ie > 9', 'last 2 versions'],
+          cascade: false
+        })
+      )
+      // .pipe(cssmin())
+      .pipe(gulp.dest('./lib/'))
+  );
+}
 // 打包文件
-gulp.task('build', ['webpack', 'compile']);
+gulp.task('build', gulp.parallel(wp, compile));
 
-// 监听文件变化
-gulp.task('watch', ['webpack', 'compile'], function () {
-  gulp.watch(['./src/**', './packages/**'], ['webpack']);
-  gulp.watch(['./styles/**'], ['compile']);
+// 监听文件变化 ["webpack", "compile"],
+gulp.task('watch', function() {
+  gulp.watch(['./src/**', './packages/**'], wp);
+  gulp.watch(['./styles/**'], compile);
 });
